2d5447a86029125c32166713b365b08984ade5a3
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-09-17 09:49:56 +0530

    api link working

--- src/Question.js
+++ src/Question.js
@@ -1,17 +1,66 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import data from "./data";
 function Question()
 {
+    // const [opt , setOpt] = React.useState(data);
+    // const optdata = data.map(item => {
+    //     <option  />
+    // })
+
+    // const[count,setCount] = React.useState(0);
+    // function kick()
+    
+    // {
+    //     setCount(prevCount => prevCount+1);
+    // }
+//   const[name,setName] = useState([]);
 
 
+//     useEffect(() => {
+//         names();
+//     })
 
-    return (
+//     const names  = () => {
 
-        
+//     const response = fetch('https://opentdb.com/api.php?amount=10');
+//      setName( response.json())
+
+//     }
+//     console.log(names);
+
+
+    useEffect(() => {
+        const url = "https://opentdb.com/api.php?amount=10";
+    
+        const fetchData = async () => {
+          try {
+            const response = await fetch(url);
+            const json = await response.json();
+            console.log(json);
+          } catch (error) {
+            console.log("error", error);
+          }
+        };
+    
+        fetchData();
+    }, []);
+
+
+    
+
+        //   <div className="quest-box">
+        //         <h2 className="quest-main">How would one say goodbye in Spanish?</h2>
+
+
+        //   </div>
+        // <>
+        // <h1 >{count}</h1>
+        // <button onClick={kick}>CLick me</button>
+        // </>
         
           
          
-    )
+
 
 
 }
--- src/data.js
+++ src/data.js
@@ -1,6 +1,23 @@
 export default [
     {
-        
+        opt : "Adios",
+        isHeld : false
+    },
+    {
+        opt : "ooos",
+        isHeld : false
+    },
+    {
+        opt : "iiios",
+        isHeld : false
+    },
+    {
+        opt : "dddios",
+        isHeld : false
+    },
+    {
+        opt : "ssss s",
+        isHeld : false
     }
   
 ]
