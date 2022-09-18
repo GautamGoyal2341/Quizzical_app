214c22e77b5703e016aef85a29cadca7445a67e9
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-09-18 14:14:59 +0530

    new api fetch method added

--- src/Question.js
+++ src/Question.js
@@ -1,68 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import data from "./data";
 function Question()
 {
-    // const [opt , setOpt] = React.useState(data);
-    // const optdata = data.map(item => {
-    //     <option  />
-    // })
 
-    // const[count,setCount] = React.useState(0);
-    // function kick()
-    
-    // {
-    //     setCount(prevCount => prevCount+1);
-    // }
-//   const[name,setName] = useState([]);
+       const[qdata , setQdata] = React.useState([]);
+      
+        fetch("https://opentdb.com/api.php?amount=30&category=12&difficulty=easy&type=multiple")
+        .then(res => res.json())
+        .then(data => setQdata(data))   
+        // console.log(setQuestData);  
 
-
-//     useEffect(() => {
-//         names();
-//     })
-
-//     const names  = () => {
-
-//     const response = fetch('https://opentdb.com/api.php?amount=10');
-//      setName( response.json())
-
-//     }
-//     console.log(names);
+        return (
+          <div>
+            <h1>hji</h1>
+            <pre>{JSON.stringify(qdata,null,2)}</pre>
+          </div>
 
 
-    useEffect(() => {
-        const url = "https://opentdb.com/api.php?amount=10";
-    
-        const fetchData = async () => {
-          try {
-            const response = await fetch(url);
-            const json = await response.json();
-            console.log(json);
-          } catch (error) {
-            console.log("error", error);
-          }
-        };
-    
-        fetchData();
-    }, []);
-
-
-    
-
-        //   <div className="quest-box">
-        //         <h2 className="quest-main">How would one say goodbye in Spanish?</h2>
-
-
-        //   </div>
-        // <>
-        // <h1 >{count}</h1>
-        // <button onClick={kick}>CLick me</button>
-        // </>
-        
-          
-         
+        )
+      
 
 
 
 }
 
 export default Question;
+
